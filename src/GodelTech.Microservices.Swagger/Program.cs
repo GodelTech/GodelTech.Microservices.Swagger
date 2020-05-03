eaa73de9fe5aca94d138f0b5d5596e8d475a4ed6
diamond_dragon <diamond_dragon@tut.by>
2020-05-03 12:54:37 +0300

    Initial commit

---
+++ src/GodelTech.Microservices.Swagger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GodelTech.Microservices.Swagger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ test/GodelTech.Microservices.Swagger.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace GodelTech.Microservices.Swagger.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
