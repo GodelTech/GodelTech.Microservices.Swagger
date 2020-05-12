88630e004abe15b5faa533bc83bd9b6cd8bfcbfc
diamond_dragon <diamond_dragon@tut.by>
2020-05-12 23:50:47 +0300

    Constant value fixes

--- src/GodelTech.Microservices.Swagger/Swagger/OAuth2Security.cs
+++ src/GodelTech.Microservices.Swagger/Swagger/OAuth2Security.cs
@@ -4,8 +4,8 @@
     {
         public static readonly string OAuth2 = "oauth2";
         public static readonly string AuthorizationCode = "oauth2-authorization-code";
-        public static readonly string Implicit;
-        public static readonly string ResourceOwnerPasswordCredentials;
-        public static readonly string ClientCredentials;
+        public static readonly string Implicit = "oauth2-implicit";
+        public static readonly string ResourceOwnerPasswordCredentials = "oauth2-password";
+        public static readonly string ClientCredentials = "oauth2-client-credentials";
     }
 }
