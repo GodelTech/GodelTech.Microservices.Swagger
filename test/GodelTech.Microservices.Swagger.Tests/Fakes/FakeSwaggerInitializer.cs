b6351f3bb8f510b20292d19c4af03b45a0e082cb
Andrei Yaskevich <andrez.yaskevich@gmail.com>
2022-01-15 16:12:14 +0300

    add ConfigureSwaggerUiOptions tests

---
+++ test/GodelTech.Microservices.Swagger.Tests/Fakes/FakeSwaggerInitializer.cs
@@ -0,0 +1,18 @@
+﻿using Swashbuckle.AspNetCore.SwaggerGen;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace GodelTech.Microservices.Swagger.Tests.Fakes
+{
+    public class FakeSwaggerInitializer : SwaggerInitializer
+    {
+        public new void ConfigureSwaggerGenOptions(SwaggerGenOptions options)
+        {
+            base.ConfigureSwaggerGenOptions(options);
+        }
+        
+        public new void ConfigureSwaggerUiOptions(SwaggerUIOptions options)
+        {
+            base.ConfigureSwaggerUiOptions(options);
+        }
+    }
+}
---
+++ test/GodelTech.Microservices.Swagger.Tests/SwaggerInitializerTests.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using GodelTech.Microservices.Swagger.Tests.Fakes;
+using Swashbuckle.AspNetCore.SwaggerUI;
+using Xunit;
+
+namespace GodelTech.Microservices.Swagger.Tests
+{
+    public class SwaggerInitializerTests
+    {
+        private readonly FakeSwaggerInitializer _initializer;
+
+        public SwaggerInitializerTests()
+        {
+            _initializer = new FakeSwaggerInitializer();
+        }
+
+        [Fact]
+        public void ConfigureSwaggerUiOptions_Success()
+        {
+            // Arrange
+            var options = new SwaggerUIOptions();
+
+            // Act
+            _initializer.ConfigureSwaggerUiOptions(options);
+
+            // Assert
+            var swaggerEndpoint = Assert.Single(options.ConfigObject.Urls);
+
+            Assert.Equal("/swagger/v1/swagger.json", swaggerEndpoint.Url);
+            Assert.Equal("v1", swaggerEndpoint.Name);
+        }
+    }
+}
