7c879ba4276f848cd32a0d24805cd1b94f8af1dd
Andrei Yaskevich <andrez.yaskevich@gmail.com>
2022-01-26 23:02:58 +0300

    add OAuth2OperationFilterHelpers and OAuth2OperationFilter tests

---
+++ test/GodelTech.Microservices.Swagger.Tests/Fakes/FakeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GodelTech.Microservices.Swagger.Tests.Fakes
+{
+    [Authorize("add")]
+    [SwaggerSecurity("Test scope")]
+    public class FakeController : ControllerBase
+    {
+        public void FakeMethod(){}
+    }
+}
---
+++ test/GodelTech.Microservices.Swagger.Tests/Filters/OAuth2OperationFilterHelpersTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using GodelTech.Microservices.Swagger.Filters;
+using Microsoft.OpenApi.Models;
+using Xunit;
+
+namespace GodelTech.Microservices.Swagger.Tests.Filters
+{
+    public class OAuth2OperationFilterHelpersTests
+    {
+        [Fact]
+        public void CreateOpenApiSecurityScheme_Success()
+        {
+            // Arrange
+            var expectedOpenApiSecurityScheme = new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Id = "Test Id",
+                    Type = ReferenceType.SecurityScheme
+                }
+            };
+
+            // Act
+            var result =
+                OAuth2OperationFilterHelpers.CreateOpenApiSecurityScheme(
+                    "Test Id");
+
+            // Assert
+            result.Should().BeEquivalentTo(expectedOpenApiSecurityScheme);
+        }
+    }
+}
